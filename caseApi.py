a8e648efd774ee0132594e8fb37c71fe5a9b7b94
Project-VER <projectver.anu@gmail.com>
2024-09-02 15:27:40 +1000

    Update caseApi.py

--- caseApi.py
+++ caseApi.py
@@ -2,12 +2,33 @@ import gpiod
 import time
 
 chip = gpiod.Chip('gpiochip4')
-
 button_desc = chip.get_line(2)
-
-#button_desc.request(consumer="Line", type=gpiod.LINE_REQ_EV_BOTH_EDGES)
 button_desc.request(consumer="Line", type=gpiod.LINE_REQ_EV_RISING_EDGE)
 
+last_press_time = 0
+press_count = 0
+timeout = 2  # seconds
+
 while True:
-	time.sleep(0.1)
-	print(button_desc.get_value())
+    if button_desc.event_wait(sec=1):
+        event = button_desc.event_read()
+        if event.type == gpiod.LineEvent.RISING_EDGE:
+            current_time = time.time()
+            if current_time - last_press_time > timeout:
+                press_count = 1
+            else:
+                press_count += 1
+            
+            last_press_time = current_time
+            
+            if press_count == 1:
+                time.sleep(0.5)  # Wait a bit to see if there's a second press
+                if press_count == 1:
+                    print("Desc")
+            elif press_count == 2:
+                print("Chat")
+                press_count = 0
+    
+    # Reset press count if timeout is reached
+    if time.time() - last_press_time > timeout:
+        press_count = 0
