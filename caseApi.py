c28f00729c742dd7c57ce2b0501017b33d4dfe3e
Project-VER <projectver.anu@gmail.com>
2024-09-02 14:24:02 +1000

    Update caseApi.py

--- caseApi.py
+++ caseApi.py
@@ -14,31 +14,36 @@ button_read.request(consumer="Button", type=gpiod.LINE_REQ_DIR_IN)
 last_desc_press = 0
 desc_press_count = 0
 debounce_time = 0.05  # 50 ms debounce time
+double_click_time = 0.4  # 400 ms for double click detection
 
 try:
     while True:
         # Check the read button
-        if button_read.get_value() == 1:
+        if button_read.get_value() == 0:
             print("read")
             time.sleep(debounce_time)
 
         # Check the desc button
-        if button_desc.get_value() == 1:
+        if button_desc.get_value() == 0:
             current_time = time.time()
             if current_time - last_desc_press > debounce_time:
-                if current_time - last_desc_press < 0.5:  # 500 ms for double press
+                if current_time - last_desc_press < double_click_time:
                     desc_press_count += 1
+                    if desc_press_count == 2:
+                        print("chat")
+                        desc_press_count = 0
                 else:
+                    if desc_press_count == 1:
+                        print("desc")
                     desc_press_count = 1
-
-                if desc_press_count == 1:
-                    print("desc")
-                elif desc_press_count == 2:
-                    print("chat")
-                    desc_press_count = 0
 
                 last_desc_press = current_time
                 time.sleep(debounce_time)
+
+        # Check if we need to reset the desc_press_count
+        if desc_press_count == 1 and time.time() - last_desc_press > double_click_time:
+            print("desc")
+            desc_press_count = 0
 
         time.sleep(0.01)  # Small delay to prevent CPU hogging
 
